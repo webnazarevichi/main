1819c3c782018203878e78d9687d2e6a08d3ffce
webnazarevichi <86548830+webnazarevichi@users.noreply.github.com>
2022-03-23 11:44:04 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+$( document ).ready(function() {
+
+    //Dropdown Menu
+    $('.dropdown').on('click', function(){
+        $('.header__dropdown_menu').toggleClass('active');
+    });
+
+	//Accordion Class Toggle
+    $('.faq__item_title').click(function(event) {
+        $('.faq__item_title').not($(this)).removeClass('active');
+        $('.faq__item_text').not($(this).next()).slideUp(300);
+        $(this).toggleClass('active').next().slideToggle(300);
+    });
+
+    //STICKY MAINMENU
+
+    $(window).scroll(function(){
+      var sticky = $('.header'),
+          scroll = $(window).scrollTop();
+
+      if (scroll > 0) sticky.addClass('fixed');
+      else sticky.removeClass('fixed');
+    });
+
+
+});
+
+
+//Contact Text Animated
+    const text = document.querySelector('.contact__text h4');
+    text.innerHTML = text.innerText.split("").map(
+        (char, i) =>
+        `<span style = "transform:rotate(${i*9}deg)">${char}</span> `
+    ).join("");
