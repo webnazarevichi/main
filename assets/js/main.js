d74ff6d99c752520c9cb33f8879dbb722b5ca908
webnazarevichi <86548830+webnazarevichi@users.noreply.github.com>
2022-09-04 14:19:04 +0300

    scale body

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,14 @@
 $( document ).ready(function() {
+	
+    if ($(window).width() > 2500) {
+ 
+        $('body').css("transform","scale(1.4)");
+     
+    } else {
+     
+        $('body').css("transform","scale(1)");
+     
+    }
 
     //Dropdown Menu
     $('.dropdown').on('click', function(){
